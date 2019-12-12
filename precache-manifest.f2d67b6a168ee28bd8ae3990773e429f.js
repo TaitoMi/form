22fa8d0e93b8bac6febc6c108ed466e0639eb400
TaitoMi <gfdaker96@gmail.com>
2019-12-12 12:28:23 +0300

    Updates

---
+++ precache-manifest.f2d67b6a168ee28bd8ae3990773e429f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155cacd8c905c5d0a5ed436e7d107022",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "e6eb2c64a2fde1514636",
+    "url": "/form/static/css/2.f4d50da8.chunk.css"
+  },
+  {
+    "revision": "f855e058c5237cb71b8e",
+    "url": "/form/static/css/main.698818f9.chunk.css"
+  },
+  {
+    "revision": "e6eb2c64a2fde1514636",
+    "url": "/form/static/js/2.389542af.chunk.js"
+  },
+  {
+    "revision": "f855e058c5237cb71b8e",
+    "url": "/form/static/js/main.0efc6051.chunk.js"
+  },
+  {
+    "revision": "dd960700fd9575b77995",
+    "url": "/form/static/js/runtime-main.a35d1ccf.js"
+  },
+  {
+    "revision": "268d82340582d167d1789ab7bcc616c7",
+    "url": "/form/static/media/finished.268d8234.mp3"
+  }
+]);
